e8299d6770e49ce5f89ccb4dcf66fafe8e697c42
GiaHuyIUH <tranhuyzaza@gmail.com>
2024-11-08 20:31:07 +0700

    add login with google

--- view/Login.jsx
+++ view/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -13,12 +13,72 @@ import { REACT_APP_API_BASE_URL } from "../utils/constant";
 import { useDispatch } from "react-redux";
 import { login } from "../store/slices/authSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+// web: 520064953188-o0orgdhelcn97rb2k1o1m5r5u6sdc3as.apps.googleusercontent.com
+// ios: 520064953188-bv8e00ehsp4el7pe1nmj8sf1hoc8sgqp.apps.googleusercontent.comexpo
+// android: 520064953188-enkpvhifd1qf7pu020o9c44ph7koa5ur.apps.googleusercontent.com
+
+WebBrowser.maybeCompleteAuthSession();
+
+const LoginWithGoogle = () => {
+  return (
+    <View>
+      <Text>Google</Text>
+    </View>
+  );
+};
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState("tranhuy12072003@gmail.com");
   const [password, setPassword] = useState("Anhbakhia3@");
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
+  const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId:
+      "520064953188-enkpvhifd1qf7pu020o9c44ph7koa5ur.apps.googleusercontent.com",
+    androidClientId:
+      "520064953188-enkpvhifd1qf7pu020o9c44ph7koa5ur.apps.googleusercontent.com",
+    iosClientId:
+      "520064953188-bv8e00ehsp4el7pe1nmj8sf1hoc8sgqp.apps.googleusercontent.com",
+    webClientId:
+      "520064953188-o0orgdhelcn97rb2k1o1m5r5u6sdc3as.apps.googleusercontent.com",
+  });
+  console.log(request, response);
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setAccessToken(response.authentication.accessToken);
+      console.log(response.authentication.accessToken);
+      accessToken && fetchGoogleUser();
+    } else if (response?.type === "error") {
+      console.error("Error:", response.error);
+    }
+  }, [response]);
+
+  async function fetchGoogleUser() {
+    if (!accessToken) return;
+
+    try {
+      const response = await axios.get(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      const userInfo = response.data;
+      setUser(userInfo);
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+    }
+  }
 
   const handleLogin = async () => {
     setLoading(true); // Set loading state to true
@@ -104,6 +164,14 @@ const Login = ({ navigation }) => {
           Sign up
         </Text>
       </Text>
+
+      <TouchableOpacity
+        style={{ justifyContent: "center", alignItems: "center" }}
+        onPress={() => promptAsync()}
+        disabled={!request}
+      >
+        <Text style={styles.googleButtonText}>Sign in with Google</Text>
+      </TouchableOpacity>
     </View>
   );
 };
